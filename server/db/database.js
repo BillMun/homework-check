4697495e90255ab4d061fe5a5bb8829b1d342941
Bill Munkacsy <billmunkacsy@gmail.com>
2022-10-13 14:03:24 -0400

    deploy

--- server/db/database.js
+++ server/db/database.js
@@ -1,4 +1,4 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://localhost:5432/homework-check', { logging: false });
+const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/homework-check', { logging: false });
 
 module.exports = db;
